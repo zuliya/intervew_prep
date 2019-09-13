6a15ef30fea6b56fa93c271a124cc08a82e26110
zuliya <zulia_shavaeva@hotmail.com>
2019-09-13 11:51:20 +0100

    ADded roation points algorithms using binary search@

---
+++ rotation_point.py
@@ -0,0 +1,26 @@
+def find_rotation_point(words):
+    first_word = words[0]
+    floor_index = 0
+    ceiling_index = len(words) - 1
+
+    while floor_index < ceiling_index:
+        # Guess a point halfway between floor and ceiling
+        guess_index = floor_index + ((ceiling_index - floor_index) // 2)
+
+        # If guess comes after first word or is the first word
+        if words[guess_index] >= first_word:
+            # Go right
+            floor_index = guess_index
+        else:
+            # Go left
+            ceiling_index = guess_index
+
+        # If floor and ceiling have converged
+        if floor_index + 1 == ceiling_index:
+            # Between floor and ceiling is where we flipped to the beginning
+            # so ceiling is alphabetically first
+            return ceiling_index
+words = ['a','b','c','a','c']
+
+words = ['a','k', 'v', 'a', 'b', 'c', 'd', 'e', 'g', 'i']
+print(find_rotation_point(words))
